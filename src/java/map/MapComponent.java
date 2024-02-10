72b2b98dd6cda2686a1e8436ef80a84ee5ca9ff3
chambaz <claire.chambaz@etu.u-paris.fr>
2024-02-10 23:39:28 +0100

    #8 forme hexagnale des tuiles

--- src/java/map/MapComponent.java
+++ src/java/map/MapComponent.java
@@ -4,7 +4,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MapComponent extends JPanel {
-    private static final int TILE_SIZE = 70; // à ajuster
+    private static final int TILE_SIZE = 70; // à changer
+    private static final int HEX_RADIUS = TILE_SIZE / 2;
+
     private Board board;
 
     public MapComponent(Board board) {
@@ -18,49 +20,67 @@ public class MapComponent extends JPanel {
 
         Tile[][] tiles = board.getTiles();
 
-        if (tiles != null) { 
+        if (tiles != null) {
             for (int i = 0; i < board.getWidth(); i++) {
                 for (int j = 0; j < board.getHeight(); j++) {
                     Tile tile = tiles[i][j];
 
                     if (tile != null) {
-                        int x = i * TILE_SIZE;
-                        int y = j * TILE_SIZE;
+                        int cx = i * TILE_SIZE + TILE_SIZE / 2;
+                        int cy = j * TILE_SIZE + TILE_SIZE / 2;
 
-                        // dessine les types de terrains
-                        TerrainType terrain = tile.getTerrain();
-                        if (terrain != null) {
-                            switch (terrain) {
-                            case FOREST:
-                                g.setColor(Color.GREEN);
-                                break;
-                            case FIELD:
-                                g.setColor(Color.YELLOW);
-                                break;
-                            case PASTURE:
-                                g.setColor(Color.CYAN);
-                                break;
-                            case BRICK:
-                                g.setColor(Color.RED);
-                                break;
-                            case MOUNTAIN:
-                                g.setColor(Color.GRAY);
-                                break;
-                            case DESERT:
-                                g.setColor(Color.LIGHT_GRAY);
-                                break;
-                        }
-                    }
-                        g.fillRect(x, y, TILE_SIZE, TILE_SIZE);
+                        Polygon hexagon = createHexagon(cx, cy);
+                        g.setColor(getTerrainColor(tile.getTerrain()));
+                        g.fillPolygon(hexagon);
 
-                        // dessine numéro
                         if (tile.getTerrain() != TerrainType.DESERT) {
                             g.setColor(Color.BLACK);
-                            g.drawString(Integer.toString(tile.getDiceNumber()), x + TILE_SIZE / 2, y + TILE_SIZE / 2);
+                            drawCenteredString(g, Integer.toString(tile.getDiceNumber()), cx, cy);
                         }
                     }
                 }
             }
         }
+    }
+
+    private Polygon createHexagon(int cx, int cy) {
+        Polygon hexagon = new Polygon();
+        for (int i = 0; i < 6; i++) {
+            double angle = Math.PI / 3 * i;
+            int x = (int) (cx + HEX_RADIUS * Math.cos(angle));
+            int y = (int) (cy + HEX_RADIUS * Math.sin(angle));
+            hexagon.addPoint(x, y);
+        }
+        return hexagon;
+    }
+
+    private Color getTerrainColor(TerrainType terrain) {
+        if (terrain != null) {
+            switch (terrain) {
+                case FOREST:
+                    return Color.GREEN;
+                case FIELD:
+                    return Color.YELLOW;
+                case PASTURE:
+                    return Color.CYAN;
+                case BRICK:
+                    return Color.RED;
+                case MOUNTAIN:
+                    return Color.GRAY;
+                case DESERT:
+                    return Color.PINK;
+                default:
+                    return Color.WHITE;
+            }
+        } else {
+            return Color.BLACK;
+        }
+    }
+
+    private void drawCenteredString(Graphics g, String text, int cx, int cy) {
+        FontMetrics metrics = g.getFontMetrics();
+        int x = cx - metrics.stringWidth(text) / 2;
+        int y = cy - metrics.getHeight() / 2 + metrics.getAscent();
+        g.drawString(text, x, y);
     }
 }
