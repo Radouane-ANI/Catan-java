b7d2f9f254f672a16c31183424bb3b24f44da596
chambaz <claire.chambaz@etu.u-paris.fr>
2024-02-21 17:16:56 +0100

    #8 couleurs

--- src/java/gui/TerrainColor.java
+++ src/java/gui/TerrainColor.java
@@ -5,9 +5,29 @@ import java.awt.Color;
 import map.TerrainType;
 
 class TerrainColor {
-    private static final Color[] TERRAIN_COLORS = {Color.GREEN, Color.YELLOW, Color.BLUE, Color.RED, Color.GRAY, Color.ORANGE};
+    private static final Color DARK_GREEN = new Color(0, 100, 0);
+    private static final Color MUSTARD_YELLOW = new Color(255, 219, 88);
+    private static final Color LIGHT_GREEN = new Color(144, 238, 144);
+    private static final Color RED_ORANGE = new Color(255, 69, 0);
+    private static final Color GREY = Color.GRAY;
+    private static final Color GREY_ORANGE = new Color(255, 140, 0);
 
     public static Color getTerrainColors(TerrainType terrain) {
-        return TERRAIN_COLORS[terrain.ordinal()];
+        switch (terrain) {
+            case FOREST:
+                return DARK_GREEN;
+            case FIELD:
+                return MUSTARD_YELLOW;
+            case PASTURE:
+                return LIGHT_GREEN;
+            case BRICK:
+                return RED_ORANGE;
+            case MOUNTAIN:
+                return GREY;
+            case DESERT:
+                return GREY_ORANGE;
+            default:
+                return Color.BLACK;
+        }
     }
 }
