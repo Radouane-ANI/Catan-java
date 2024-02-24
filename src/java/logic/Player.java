0b4be2a5916083cee9dd81c9815186e8c4a30144
Radouane Aouini <radouaneloo.aouini@gmail.com>
2024-02-24 15:20:08 +0100

    fix bug

--- src/java/logic/Player.java
+++ src/java/logic/Player.java
@@ -2,6 +2,7 @@ package logic;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 
 import static logic.Card.*;
@@ -167,6 +168,7 @@ public class Player implements Trade {
         wishList.clearBox();
         HashMap<Card, Integer> etats = getMissingCardsForBuildings();
         List<Card> keys = new ArrayList<>(etats.keySet());
+        Collections.shuffle(keys);
 
         Card min = keys.get(0);
         Card max = keys.get(0);
