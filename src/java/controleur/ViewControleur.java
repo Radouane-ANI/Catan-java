ffa1b8697aa2bfa42e575bbd07f9e2902ca04282
chambaz <claire.chambaz@etu.u-paris.fr>
2024-03-16 15:03:18 +0100

    #17 ajout dans le test de la constrction des  premiers éléments

--- src/java/controleur/ViewControleur.java
+++ src/java/controleur/ViewControleur.java
@@ -1,8 +1,11 @@
 package src.java.controleur;
 
+import java.awt.Color;
+
 import src.java.gui.GameMenu;
 import src.java.gui.GameView;
 import src.java.gui.MainFrame;
+import src.java.logic.Player;
 import src.java.map.Board;
 import src.java.gui.CatanBoardView;
 
@@ -11,6 +14,7 @@ public class ViewControleur {
 
     private static MainFrame frame;
     private static CatanBoardControleur catanControleur;
+    private static Player player;
 
     public ViewControleur(MainFrame mainFrame) {
         frame = mainFrame;
@@ -28,6 +32,9 @@ public class ViewControleur {
 
         CatanBoardView mapComponent = new CatanBoardView(gameView.getSize());
         catanControleur = new CatanBoardControleur(mapComponent);
+
+        player = new Player(false, "Sam", null, Color.BLUE);
+        catanControleur.firstBuild(player);;
 
         gameView.add(mapComponent);
 
