537fae8adc002c14da1a27f37072a16ea1fcd88b
Zhula <zhula9627@gmail.com>
2024-05-19 18:01:46 +0200

    up

--- src/java/controleur/Turn.java
+++ src/java/controleur/Turn.java
@@ -3,12 +3,9 @@ package controleur;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+import logic.*;
 import map.Node;
-import logic.Player;
-import logic.City;
-import logic.HumanGroup;
-import logic.Settlement;
-import logic.Thief;
 import map.Board;
 import map.Tile;
 import util.TerrainType;
@@ -76,6 +73,17 @@ public class Turn {
         } else {
             actionBot.firstBuild(player);
         }
+    }
+
+    public int mono_getCardFromEveryone(Card c) {
+        int counter = 0;
+        for(Player player : playersList) {
+            if (player != currentPlayer) {
+                counter = player.getMyCards().getNumber(c);
+                player.getMyCards().setZero(c);
+            }
+        }
+        return counter;
     }
 
 
